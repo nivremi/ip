1bfbfc111a072db5a893e915df05b790221b088a
nivremi <mervinang.57@gmail.com>
2026-08-20 16:02:21 +0800

    Level 0 tasks done

---
+++ src/main/java/Rei.java
@@ -0,0 +1,17 @@
+public class Rei {
+    public static void main(String[] args) {
+        String banner = "██████╗ ███████╗██╗\n" +
+                "██╔══██╗██╔════╝██║\n" +
+                "██████╔╝█████╗  ██║\n" +
+                "██╔══██╗██╔══╝  ██║\n" +
+                "██║  ██║███████╗██║\n" +
+                "╚═╝  ╚═╝╚══════╝╚═╝\n";
+        System.out.println(banner);
+        System.out.println("____________________________________________________________");
+        System.out.println("Hey there, my name is Rei!");
+        System.out.println("I will do my best to assist you!");
+        System.out.println("____________________________________________________________");
+        System.out.println("See you soon and have a nice day ahead! :D");
+        System.out.println("____________________________________________________________");
+    }
+}
